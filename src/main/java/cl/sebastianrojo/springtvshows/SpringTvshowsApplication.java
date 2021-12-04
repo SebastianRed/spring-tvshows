7acfdd4d81bd1f61ad1e911a03fc4f119272d609
Sebastián Rojo <sebastianrojocollao@gmail.com>
2021-12-04 16:47:48 -0300

    Commit inicial

---
+++ src/main/java/cl/sebastianrojo/springtvshows/SpringTvshowsApplication.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.springtvshows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTvshowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTvshowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sebastianrojo/springtvshows/SpringTvshowsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.springtvshows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTvshowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
